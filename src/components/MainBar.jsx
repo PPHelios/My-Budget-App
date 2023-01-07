3c0fabb7054e943a217aeaef4b372cb38ad82141
PPHelios <pphelios1@gmail.com>
2023-01-08 00:33:35 +0200

    main bar fix

--- src/components/MainBar.jsx
+++ src/components/MainBar.jsx
@@ -1,32 +1,34 @@
 import Button from "@mui/material/Button";
 import Stack from "@mui/material/Stack";
-import Typography from '@mui/material/Typography'
-import AppBar from '@mui/material/AppBar'
-import Toolbar from '@mui/material/Toolbar'
+import Typography from "@mui/material/Typography";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
 export const MainBar = () => {
   return (
-   <AppBar position="static" color="primary">
-     <Toolbar>
-       <Typography variant="h6" sx={{ flexGrow: 1, fontStyle: 'italic'  }}>
-         My Budget
-       </Typography>
-       <Stack
-        direction="row"
-        justifyContent="center"
-        alignItems="center"
-        spacing={2}
-      >
-        <Button variant="contained" color="secondary" size="small">
-          Add Budget
-        </Button>
-        <Button variant="contained" color="primary" size="small">
-          Add Expense
-        </Button>
-      </Stack>
-     </Toolbar>
-   </AppBar>
-      
-
-  
-);
-}
+    <AppBar position="static" color="primary">
+      <Toolbar>
+        <Typography variant="h6" sx={{ flexGrow: 1, fontStyle: "italic" }}>
+          My Budget
+        </Typography>
+        <Stack
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+          spacing={2}
+        >
+          <Button
+            variant="contained"
+            color="secondary"
+            size="large"
+            sx={{ sm: { fontSize: 5, color: "red" } }}
+          >
+            Add Budget
+          </Button>
+          <Button variant="contained" color="primary" size="small">
+            Add Expense
+          </Button>
+        </Stack>
+      </Toolbar>
+    </AppBar>
+  );
+};
